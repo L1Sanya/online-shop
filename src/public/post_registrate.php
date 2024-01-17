5292fc858be7674af1eb687deeb0130876fad163
alex <dashiev.2003@mail.ru>
2024-01-18 00:08:46 +0900

    Added Validating UsersData

--- src/public/post_registrate.php
+++ src/public/post_registrate.php
@@ -1,6 +1,10 @@
 <?php
+$pdo = new PDO("pgsql:host=database;port=5432;dbname=testdb", "alex", "2612");
 
-$name = $_POST['name'];
+
+$validationError = false;
+
+
 if (isset($_POST['name']))
 {
     $name = $_POST['name'];
@@ -9,25 +13,61 @@ if (isset($_POST['name']))
 if (empty($name))
 {
     echo 'Имя должно быть заполнено';
+    $validationError = true;
 }
 
 if (strlen($name) < 2)
 {
     echo 'Имя должно быть больше 2 символов';
+    $validationError = true;
 }
 
-$email = $_POST['email'];
+
+if (isset($_POST['email']))
+{
+    $email = $_POST['email'];
+}
+
+if (empty($email))
+{
+    echo 'email должно быть заполнено';
+    $validationError = true;
+}
+
+if (filter_var($email, FILTER_VALIDATE_EMAIL) === false)
+{
+    echo 'Email указан не правильно';
+    $validationError = true;
+}
 
 
-$password = $_POST['psw'];
+if (isset($_POST['psw']))
+{
+    $password = $_POST['psw'];
+}
+
+if (empty($password))
+{
+    echo 'Пароль должен быть заполнен';
+    $validationError = true;
+}
+
+if (strlen($password) < 7)
+{
+    echo 'Пароль должен быть длинной не менее 8 символов';
+    $validationError = true;
+}
+
 $passwordRepeat = $_POST['psw-repeat'];
 
+if ($password !== $passwordRepeat)
+{
+    echo 'Пароль не совпадает';
+    $validationError = true;
+}
 
-$pdo = new PDO("pgsql:host=database;port=5432;dbname=testdb", "alex", "2612");
-
-$stmt = $pdo->prepare('INSERT INTO users (name, email, password) VALUES (:name, :email, :password');
-$stmt->execute(['name' => $name, 'email' => $email, 'password' => $password]);
-
-
-
-$pdo->exec("INSERT INTO users (name, email, password) VALUES ('$name','$email','$password')");
+if ($validationError === false)
+{
+    $stmt = $pdo->prepare('INSERT INTO users (name, email, password) VALUES (:name, :email, :password)');
+    $stmt->execute(['name' => $name, 'email' => $email, 'password' => $password]);
+}
